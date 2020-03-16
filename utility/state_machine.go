5d4a29710b1c57ef1b12b4ed7d72fb91659ac779
adam <wjw3323@live.com>
2020-03-16 23:31:03 +0800

    update state machine

--- utility/state_machine.go
+++ utility/state_machine.go
@@ -1,5 +1,11 @@
 package utility
 
+import "errors"
+
+var(
+	StateNotFound = errors.New("state not found in the machine")
+)
+
 type State interface {
 	String() string
 	IsEnd() bool
@@ -8,13 +14,35 @@ type State interface {
 	//Arg() interface{}
 }
 
-type Event string
+type Process func(interface{}) (State, error)
 
 type StateMachine struct {
 	currentState State
 	lastState    State
+	stateTable   map[string]Process
 }
 
-func (stateMachine *StateMachine) addEvent(from State, event Event, to State) {
 
+
+func (stateMachine *StateMachine) Register(from State, process Process) {
+	if stateMachine.stateTable == nil {
+		stateMachine.stateTable = make(map[string]Process)
+	}
+	stateMachine.stateTable[from.String()] = process
+}
+
+func (stateMachine *StateMachine) Current() State {
+	return stateMachine.currentState
+}
+
+func (stateMachine *StateMachine) Last() State {
+	return stateMachine.lastState
+}
+
+func (stateMachine *StateMachine) Fire(arg interface{}) (State, error) {
+	 process , ok:= stateMachine.stateTable[stateMachine.currentState.String()]
+	if ok {
+		return process(arg)
+	}
+	return nil, StateNotFound
 }
